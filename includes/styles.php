7a462a69ae829657b59d3d424556c461b51fa1d3
James McHugh <james.mchugh1988@gmail.com>
2019-11-15 15:53:26 +0000

    import jquery, bottstrap and fontawesome

---
+++ includes/styles.php
@@ -0,0 +1,4 @@
+<link type="text/css" rel="stylesheet" href="plugins/bootstrap/css/bootstrap.css" />
+<link type="text/css" rel="stylesheet" href="plugins/fontawesome/css/all.css" />
+<link type="text/css" rel="stylesheet" href="css/overrides.css" />
+<script type="text/javascript" src="plugins/jquery/jquery-3.4.1.js"></script>
