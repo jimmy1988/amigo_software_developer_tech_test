83cb661d00abaee07375af6732c0747bdde67012
James McHugh <james.mchugh1988@gmail.com>
2019-11-17 01:18:07 +0000

    ew weather api key and url to call

--- php/config.php
+++ php/config.php
@@ -1,3 +1,7 @@
 <?php
   !defined("APP_NAME") ? define("APP_NAME", "Amigo Technicak Test Weather Widget") : APP_NAME;
+  !defined("OPEN_WEATHER_API_KEY") ? define("OPEN_WEATHER_API_KEY", "b20cbc011889511f1f87b8c7e9ee5a5e") : OPEN_WEATHER_API_KEY;
+  !defined("OPEN_WEATHER_API_URL") ? define("OPEN_WEATHER_API_URL", "api.openweathermap.org/data/2.5/weather?") : OPEN_WEATHER_API_URL;
+
+
 ?>
