d54806901195cfc2f289fe9da09601ae95ec53b2
James McHugh <james.mchugh1988@gmail.com>
2019-11-18 01:29:51 +0000

    added file type and url for the weather image

--- php/config.php
+++ php/config.php
@@ -2,6 +2,8 @@
   !defined("APP_NAME") ? define("APP_NAME", "Amigo Technicak Test Weather Widget") : APP_NAME;
   !defined("OPEN_WEATHER_API_KEY") ? define("OPEN_WEATHER_API_KEY", "b20cbc011889511f1f87b8c7e9ee5a5e") : OPEN_WEATHER_API_KEY;
   !defined("OPEN_WEATHER_API_URL") ? define("OPEN_WEATHER_API_URL", "http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=" . OPEN_WEATHER_API_KEY . "&") : OPEN_WEATHER_API_URL;
+  !defined("OPEN_WEATHER_IMAGE_URL") ? define("OPEN_WEATHER_IMAGE_URL", "http://openweathermap.org/img/wn/") : OPEN_WEATHER_IMAGE_URL;
+  !defined("OPEN_WEATHER_IMAGE_TYPE") ? define("OPEN_WEATHER_IMAGE_TYPE", ".png") : OPEN_WEATHER_IMAGE_TYPE;
   !defined("IP_INFO_URL") ? define("IP_INFO_URL", "http://ipinfo.io/") : IP_INFO_URL;
 
 
