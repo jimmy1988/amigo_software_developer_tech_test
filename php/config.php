4035de68f5d3ed5ed90905d34fdbc45b15f51fa9
James McHugh <james.mchugh1988@gmail.com>
2019-11-18 00:29:07 +0000

    added additional config varaibles

--- php/config.php
+++ php/config.php
@@ -1,7 +1,8 @@
 <?php
   !defined("APP_NAME") ? define("APP_NAME", "Amigo Technicak Test Weather Widget") : APP_NAME;
   !defined("OPEN_WEATHER_API_KEY") ? define("OPEN_WEATHER_API_KEY", "b20cbc011889511f1f87b8c7e9ee5a5e") : OPEN_WEATHER_API_KEY;
-  !defined("OPEN_WEATHER_API_URL") ? define("OPEN_WEATHER_API_URL", "api.openweathermap.org/data/2.5/weather?") : OPEN_WEATHER_API_URL;
+  !defined("OPEN_WEATHER_API_URL") ? define("OPEN_WEATHER_API_URL", "http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=" . OPEN_WEATHER_API_KEY . "&") : OPEN_WEATHER_API_URL;
+  !defined("IP_INFO_URL") ? define("IP_INFO_URL", "http://ipinfo.io/") : IP_INFO_URL;
 
 
 ?>
